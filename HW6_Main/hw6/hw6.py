3ecaa6b8fc2b66401f8dfacc68f6fa96d37c4e9d
Daniel Alfasi <daniel.alfasi01@post.runi.ac.il>
2023-06-25 15:03:20 +0300

    hw6 starter

---
+++ HW6_Main/hw6/hw6.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+def get_random_centroids(X, k):
+    '''
+    Each centroid is a point in RGB space (color) in the image. 
+    This function should uniformly pick `k` centroids from the dataset.
+    Input: a single image of shape `(num_pixels, 3)` and `k`, the number of centroids. 
+    Notice we are flattening the image to a two dimentional array.
+    Output: Randomly chosen centroids of shape `(k,3)` as a numpy array. 
+    '''
+    centroids = []
+    ###########################################################################
+    # TODO: Implement the function.                                           #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    # make sure you return a numpy array
+    return np.asarray(centroids).astype(np.float) 
+
+def lp_distance(X, centroids, p=2):
+    '''
+    Inputs: 
+    A single image of shape (num_pixels, 3)
+    The centroids (k, 3)
+    The distance parameter p
+
+    output: numpy array of shape `(k, num_pixels)` thats holds the distances of 
+    all points in RGB space from all centroids
+    '''
+    distances = []
+    k = len(centroids)
+    ###########################################################################
+    # TODO: Implement the function.                                           #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return distances
+
+def kmeans(X, k, p ,max_iter=100):
+    """
+    Inputs:
+    - X: a single image of shape (num_pixels, 3).
+    - k: number of centroids.
+    - p: the parameter governing the distance measure.
+    - max_iter: the maximum number of iterations to perform.
+
+    Outputs:
+    - The calculated centroids as a numpy array.
+    - The final assignment of all RGB points to the closest centroids as a numpy array.
+    """
+    classes = []
+    centroids = get_random_centroids(X, k)
+    ###########################################################################
+    # TODO: Implement the function.                                           #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return centroids, classes
+
+def kmeans_pp(X, k, p ,max_iter=100):
+    """
+    Your implenentation of the kmeans++ algorithm.
+    Inputs:
+    - X: a single image of shape (num_pixels, 3).
+    - k: number of centroids.
+    - p: the parameter governing the distance measure.
+    - max_iter: the maximum number of iterations to perform.
+
+    Outputs:
+    - The calculated centroids as a numpy array.
+    - The final assignment of all RGB points to the closest centroids as a numpy array.
+    """
+    classes = None
+    centroids = None
+    ###########################################################################
+    # TODO: Implement the function.                                           #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return centroids, classes
