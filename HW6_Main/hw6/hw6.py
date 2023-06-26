96e7a0d1338a6f0c2d60686dac23f4fb66c86f3f
Daniel Alfasi <daniel.alfasi01@post.runi.ac.il>
2023-06-26 14:24:18 +0300

    First sketch of hw6 and hw5 docx

--- HW6_Main/hw6/hw6.py
+++ HW6_Main/hw6/hw6.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def get_random_centroids(X, k):
     '''
     Each centroid is a point in RGB space (color) in the image. 
@@ -8,16 +9,11 @@ def get_random_centroids(X, k):
     Notice we are flattening the image to a two dimentional array.
     Output: Randomly chosen centroids of shape `(k,3)` as a numpy array. 
     '''
-    centroids = []
-    ###########################################################################
-    # TODO: Implement the function.                                           #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
-    # make sure you return a numpy array
-    return np.asarray(centroids).astype(np.float) 
+    centroids = np.array((k, 3))
+    random_indexs_for_centroids = np.random.randint(0, X.shape[0], size=k)
+    centroids = X[random_indexs_for_centroids]
+    return np.asarray(centroids).astype(float)
+
 
 def lp_distance(X, centroids, p=2):
     '''
@@ -29,18 +25,17 @@ def lp_distance(X, centroids, p=2):
     output: numpy array of shape `(k, num_pixels)` thats holds the distances of 
     all points in RGB space from all centroids
     '''
-    distances = []
     k = len(centroids)
-    ###########################################################################
-    # TODO: Implement the function.                                           #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
+    distances = np.zeros((k, X.shape[0]))
+
+    for i, centroid in enumerate(centroids):
+        distances[i, :] = (np.sum(np.absolute(
+            X-centroid)**p, axis=1, keepdims=True)**(1/p)).T
+
     return distances
 
-def kmeans(X, k, p ,max_iter=100):
+
+def kmeans(X, k, p, max_iter=100):
     """
     Inputs:
     - X: a single image of shape (num_pixels, 3).
@@ -52,18 +47,34 @@ def kmeans(X, k, p ,max_iter=100):
     - The calculated centroids as a numpy array.
     - The final assignment of all RGB points to the closest centroids as a numpy array.
     """
-    classes = []
     centroids = get_random_centroids(X, k)
-    ###########################################################################
-    # TODO: Implement the function.                                           #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
+
+    return kmeans_with_given_centroids(X, k, p, max_iter, centroids)
+
+
+def kmeans_with_given_centroids(X, k, p, max_iter, centroids):
+    recomputed_centroids = np.copy(centroids)
+
+    for iteration_index in range(max_iter):
+        distances_from_centroids = lp_distance(X, centroids, p)
+
+        # Find the minimum index for each row based on the lambda expression
+        classes = np.argmin(distances_from_centroids, axis=0)
+
+        for i in range(k):
+            instances_for_centroid = X[classes == i]
+            recomputed_centroids[i, :] = np.mean(
+                instances_for_centroid, axis=0)
+
+        if np.array_equal(centroids, recomputed_centroids):
+            break
+        else:
+            centroids = recomputed_centroids
+
     return centroids, classes
 
-def kmeans_pp(X, k, p ,max_iter=100):
+
+def kmeans_pp(X, k, p, max_iter=100):
     """
     Your implenentation of the kmeans++ algorithm.
     Inputs:
@@ -77,12 +88,39 @@ def kmeans_pp(X, k, p ,max_iter=100):
     - The final assignment of all RGB points to the closest centroids as a numpy array.
     """
     classes = None
-    centroids = None
-    ###########################################################################
-    # TODO: Implement the function.                                           #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
+
+    # Initialize the centroids list and choose one centroid randomly
+    # Choose first centroid
+    centroid_idx = np.random.choice(X.shape[0])
+    centroids = [X[centroid_idx]]
+
+    # Create a new array without the chosen centroid
+    mask = np.arange(X.shape[0]) != centroid_idx
+    X_without_centroids = X[mask]
+
+    # Repeat until we get k centroids
+    for _ in range(k - 1):
+
+        dist_sq = np.sum(
+            (X_without_centroids[:, np.newaxis] - centroids) ** 2, axis=2)
+        dist_sq = dist_sq.reshape(X_without_centroids.shape[0], len(centroids))
+        min_dist_sq = np.min(dist_sq, axis=1)
+
+        # compute the probabilities
+        weights = min_dist_sq / np.sum(min_dist_sq)
+
+        # add a new centroid
+        centroid_idx = np.random.choice(
+            range(X_without_centroids.shape[0]), p=weights)
+        centroids.append(X_without_centroids[centroid_idx])
+
+        mask = np.arange(X_without_centroids.shape[0]) != centroid_idx
+        X_without_centroids = X_without_centroids[mask]
+
+    # Convert list of centroids to array
+    centroids = np.asarray(centroids).astype(float)
+
+    centroids, classes = kmeans_with_given_centroids(
+        X, k, p, max_iter, centroids)
+
     return centroids, classes
